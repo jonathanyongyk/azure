75569bab62e537eb117859111dc72ee7d3a46d6e
Jonathan <dataerror_geo@hotmail.com>
2025-02-26 18:19:20 +0800

    add PS script to list all app service plan in a subscription

---
+++ AppService/GetAll-Asp.ps1
@@ -0,0 +1,59 @@
+param (
+    [Parameter(Mandatory = $true)]
+    [string]$subscriptionId
+)
+
+# Login to Azure
+#Connect-AzAccount
+
+# Set the subscription
+Set-AzContext -SubscriptionId $subscriptionId
+
+# Get the subscription details
+$subscription = Get-AzContext
+
+# Get all resource groups in the subscription
+$resourceGroups = Get-AzResourceGroup
+
+# Initialize an array to hold the results
+$allAsp = @()
+$allWebs = @()
+
+foreach ($resourceGroup in $resourceGroups) {
+    # Get all app service plans in the resource group
+    $appServicePlans = Get-AzAppServicePlan -ResourceGroupName $resourceGroup.ResourceGroupName
+        
+    foreach ($appServicePlan in $appServicePlans) {
+        $raw = (az appservice plan show --name $appServicePlan.Name -g $resourceGroup.ResourceGroupName) | out-string
+        $asp = convertfrom-json $raw
+        # Add the details to the allAsp array
+        $allAsp += [PSCustomObject]@{
+            SubscriptionId   = $subscription.Subscription.Id
+            SubscriptionName = $subscription.Subscription.Name
+            ResourceGroupName = $resourceGroup.ResourceGroupName
+            ResourceName     = $appServicePlan.Name
+            Plan             = $appServicePlan.Sku.Name
+            ZoneRedundant    = if ($asp.properties.ZoneRedundant) { "Enabled" } else { "Not Enabled" }
+            ResourceId       = $appServicePlan.Id            
+        }
+
+        # Get all web apps in the app service plan
+        $webApps = Get-AzWebApp -ResourceGroupName $resourceGroup.ResourceGroupName | Where-Object { $_.ServerFarmId -eq $appServicePlan.Id }
+
+        foreach ($webApp in $webApps) {
+            # Add the details to the allWebs array
+            $allWebs += [PSCustomObject]@{
+                ResourceGroupName = $resourceGroup.ResourceGroupName
+                WebAppName       = $webApp.Name
+                AppServicePlan   = $appServicePlan.Name
+                WebAppState      = $webApp.State
+                ResourceId       = $webApp.Id
+            }
+        }
+
+    }
+}
+
+# Export the results to a CSV file
+$allAsp | Export-Csv -Path "./appserviceplans.csv" 
+$allWebs | Export-Csv -Path "./webapps.csv"
---
+++ AppService/readme.md
@@ -0,0 +1,26 @@
+# Overview
+This script list out all the App Service Plan in the given Azure subscription and all web apps in the App Service Plan.
+The script was written to help me discover if a App Service Plan has Availability Zone enabled as part of my Azure resiliency project. But it can be enhanced/adapt to discover other properties of the App Service Plan and Web Apps or other Azure resources.
+
+# Prerequisites
+- Azure Powershell
+- Azure CLI
+
+Note
+> Most of the scripts is written in Powershell except for the code to get the Zone Redundancy setting of the App Service Plan which is written in Azure CLI. The reason is Azure Powershell does not project this property.
+
+
+## Install Azure Powershell
+You can install Azure Powershell using *Install-PSResource -Name Az -Scope <AllUsers/CurrentUser>*
+
+
+# Authentication
+You need to run *Connect-AzAccount* to authenticate to Azure first before running this script. Alternatively, you can also uncomment the *Connect-AzAccount* command in the script to authenticate to Azure.
+
+# Arguments
+This script require one argument which is the subscription id. You may pass to the script via *-subscriptionId* argument.
+
+# Output
+The script will generate two CSV files in the current directory. The first file is the list of App Service Plan (**AppServicePlan.csv**) and the second file is the list of Web Apps in the App Service Plan (**WebApp.csv**).
+You can then open the files in Excel for further analysis .
+
